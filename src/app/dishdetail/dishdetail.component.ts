8f60f42a6254877da41ebc2434e612afdc36eab0
格莱 <zhouchang29@gmail.com>
2019-08-09 12:27:22 +0800

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -63,8 +63,9 @@ export class DishdetailComponent implements OnInit {
 	private location: Location) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dish = this.dishservice.getDish(id);
+		let id = this.route.snapshot.params['id'];
+		this.dishservice.getDish(id)
+		.then(dish => this.dish = dish);
   }
 
   goBack(): void {
